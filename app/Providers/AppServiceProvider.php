c22634264f32eb7332332bbc3108b4ccea02d09a
AnandGithub2 <anandlancer.official2@gmail.com>
2026-07-17 21:07:09 +0530

    Fix Render database boot error

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,25 +5,24 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use App\Models\Setting;
 use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
-   public function boot(): void
-{
-    View::share(
-        'appSetting',
-        Setting::first()
-    );
-}
+    public function boot(): void
+    {
+        if (Schema::hasTable('settings')) {
+            View::share(
+                'appSetting',
+                Setting::first()
+            );
+        } else {
+            View::share('appSetting', null);
+        }
+    }
 }
